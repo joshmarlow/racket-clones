ee07caad84ad7ead02516faf4b379702abc3c122
Josh Marlow <joshmarlow@gmail.com>
2013-01-18 12:03:35 -0500

    * Fixed a bug with the port number argument ** Added a '-p' flag ** Changed the 'handle' function to 'echo-handler' and ** set up the server and client code to accept an 'io-handler' ** argument which is 'echo-handler' by default.

--- nc/nc.rkt
+++ nc/nc.rkt
@@ -5,32 +5,7 @@
 
 (require racket/cmdline)
 
-(define (parse-cmd-options)
-  (define (make-defaults-ht)
-    ; define some default behavior in case we don't get any parameters
-    (let ((ht (make-hash)))
-      (hash-set! ht 'mode 'client)
-      (hash-set! ht 'host "localhost")
-      (hash-set! ht 'port 80)
-      ht))
-
-  ; parse the command line options so the program knows what to do
-  (let ((cmd-options (make-defaults-ht)))
-    (command-line
-     #:program "nc"
-     #:once-each
-     [("-l" "--listen") "Run as a server"
-                        (hash-set! cmd-options 'mode `server)]
-     #:args
-     arg-list (and (if (> (length arg-list) 0)
-                    (hash-set! cmd-options 'host (first arg-list))
-                    null)
-                   (if (> (length arg-list) 1)
-                     (hash-set! cmd-options 'port (first (first arg-list)))
-                     null)))
-    cmd-options))
-
-(define (handle in-1 out-1 in-2 out-2)
+(define (echo-handler in-1 out-1 in-2 out-2)
   ; Cross the ports so that the in-1 -> out-2 and in-2 _> out-1
   (define cust (make-custodian))
   (parameterize ([current-custodian cust])
@@ -51,18 +26,48 @@
       (thread-wait p1)
       (thread-wait p2)))
 
-(define (launch-as-server host port)
+(define (parse-cmd-options)
+  (define (make-defaults-ht)
+    ; define some default behavior in case we don't get any parameters
+    (let ((ht (make-hash)))
+      (hash-set! ht 'mode 'client)
+      (hash-set! ht 'host "localhost")
+      (hash-set! ht 'port 80)
+      (hash-set! ht 'io-handler echo-handler)
+      ht))
+
+  ; parse the command line options so the program knows what to do
+  (let ((cmd-options (make-defaults-ht)))
+    (command-line
+     #:program "nc"
+     #:once-each
+     [("-l" "--listen") "Run as a server"
+                     (hash-set! cmd-options 'mode `server)]
+     [("-p" "--port") port-number
+                      "Specify a port explicitly"
+                     (hash-set! cmd-options 'port (string->number port-number))]
+     #:args
+     arg-list (and (if (> (length arg-list) 0)
+                     (hash-set! cmd-options 'host (first arg-list))
+                     #t)
+                   (if (> (length arg-list) 1)
+                     (hash-set! cmd-options 'port
+                                (string->number (first (rest arg-list))))
+                     #t)))
+    cmd-options))
+
+(define (launch-as-server host port io-handler)
     ; Launch application in Server mode
     (define listener (tcp-listen port 1 #t))
     (define-values (in out) (tcp-accept listener))
-    (handle in out (current-input-port) (current-output-port))
+    (io-handler in out (current-input-port) (current-output-port))
     (close-input-port in)
     (close-output-port out))
 
-(define (launch-as-client host port)
+(define (launch-as-client host port io-handler)
     ; Launch application in client mode
     (define-values (in out) (tcp-connect host port))
-    (handle in out (current-input-port) (current-output-port))
+    (io-handler in out (current-input-port) (current-output-port))
     (close-input-port in)
     (close-output-port out))
 
@@ -71,8 +76,12 @@
     (let ((cmd-options (parse-cmd-options)))
       (cond
         ((eq? (hash-ref cmd-options 'mode) 'server)
-         (launch-as-server(hash-ref cmd-options 'host) (hash-ref cmd-options 'port)))
+         (launch-as-server(hash-ref cmd-options 'host)
+                           (hash-ref cmd-options 'port)
+                           (hash-ref cmd-options 'io-handler)))
         ((eq? (hash-ref cmd-options 'mode) 'client)
-         (launch-as-client (hash-ref cmd-options 'host) (hash-ref cmd-options 'port))))))
+         (launch-as-client (hash-ref cmd-options 'host)
+                           (hash-ref cmd-options 'port)
+                           (hash-ref cmd-options 'io-handler))))))
 
 (main)
